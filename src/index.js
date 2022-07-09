f763645da0b63d1484c2fafb1cc4a6d30265540d
Liubov555 <101938652+Liubov555@users.noreply.github.com>
2022-07-09 06:07:42 +0300

    add city api coordinates

--- src/index.js
+++ src/index.js
@@ -56,6 +56,12 @@ form.addEventListener("submit", formInput);
 
 let celsiusTemperature = null;
 
+function getForecast(coordinates) {
+    let apiKey = "09ed621ee119cafcd8c4f28ce97c149a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
+
 function showWeather(response) {
     console.log(response.data);
     document.querySelector("#displayedCity").innerHTML = response.data.name;
@@ -70,6 +76,7 @@ function showWeather(response) {
 
     celsiusTemperature = response.data.main.temp;
 
+    getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -131,7 +138,7 @@ celsiusLink.addEventListener("click", convertToCelsius);
 
 /*Weather for the week */
 
-function displayForecast() {
+function displayForecast(response) {
     let forecastElement = document.querySelector("#forecast");
     let forecastHTML = `<div class="row">`;
     let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
@@ -158,4 +165,4 @@ function displayForecast() {
     forecastElement.innerHTML = forecastHTML;
 }
 
-displayForecast();
+
